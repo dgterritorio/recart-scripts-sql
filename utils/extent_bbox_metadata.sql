462e9b183a5a17f7c37933d839c4fd7a1b2bdf81
hsilva321 <hsilva321@gmail.com>
2022-09-01 15:46:55 +0100

    Metadados - Extensão BBOX

---
+++ utils/extent_bbox_metadata.sql
@@ -0,0 +1,2 @@
+with b as (select ST_Transform(ST_SetSRID(st_extent (public.area_trabalho.geometria),3763),4326) as bb  from public.area_trabalho)
+select st_xmin(bb) as longitude_oeste,st_ymin(bb) as latitude_sul, st_xmax(bb) as longitude_este,st_ymax(bb) as latitude_norte from b;
